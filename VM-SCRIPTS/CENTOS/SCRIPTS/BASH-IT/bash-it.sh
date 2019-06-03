25d3b3f42aeffdccf3e05be31de6024be9390c41
Ronny Karallus <1786550+rkarallus@users.noreply.github.com>
2019-06-03 17:01:57 +0200

    Update bash-it.sh

--- VM-SCRIPTS/CENTOS/SCRIPTS/BASH-IT/bash-it.sh
+++ VM-SCRIPTS/CENTOS/SCRIPTS/BASH-IT/bash-it.sh
@@ -4,3 +4,5 @@ yum -y install git
 
 su developer -c 'git clone --depth=1 https://github.com/Bash-it/bash-it.git /home/developer/.bash_it'
 su developer -c '/home/developer/.bash_it/install.sh --silent'
+
+echo 'export EDITOR="/usr/bin/vim"' >> /home/developer/.bashrc
