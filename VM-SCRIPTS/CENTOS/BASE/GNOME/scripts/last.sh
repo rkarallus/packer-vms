503164eeed6cd46b6619f19a10e2c964091a714e
Ronny Karallus <ronny.karallus@gmail.com>
2019-11-29 19:03:59 +0100

    cleanup

--- VM-SCRIPTS/CENTOS/BASE/GNOME/scripts/last.sh
+++ VM-SCRIPTS/CENTOS/BASE/GNOME/scripts/last.sh
@@ -5,3 +5,5 @@ su developer -c 'dbus-launch gsettings set org.gnome.desktop.session idle-delay 
 su developer -c 'dbus-launch gsettings set org.gnome.desktop.screensaver lock-delay 0'
 su developer -c 'dbus-launch gsettings set org.gnome.desktop.screensaver lock-enabled false'
 su developer -c 'dbus-launch gsettings set org.gnome.desktop.interface enable-animations false'
+
+sudo yum -y erase empathy.x86_64 cheese.x86_64
