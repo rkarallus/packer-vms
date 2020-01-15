f7ebb0ce9b2ca0bf938b7ca9ece4454d2963bba4
Ronny Karallus <ronny.karallus@gmail.com>
2020-01-15 01:03:08 +0100

    adding gradle and maven

--- VM-SCRIPTS/CENTOS/FULL/INTEGRATE-ALL/scripts/executeAll.sh
+++ VM-SCRIPTS/CENTOS/FULL/INTEGRATE-ALL/scripts/executeAll.sh
@@ -3,6 +3,7 @@ find /tmp/update-scripts/ -type f -print0 | xargs -0 dos2unix
 find /tmp/update-scripts/ -type f -print0 | xargs -0 chmod +x
 
 sudo /tmp/update-scripts/JDK/jdk.sh
+sudo /tmp/update-scripts/MAVEN/maven.sh
 sudo /tmp/update-scripts/NODE/node.sh
 sudo /tmp/update-scripts/ATOM/atom.sh
 sudo /tmp/update-scripts/DOCKER/docker.sh
@@ -14,6 +15,7 @@ sudo /tmp/update-scripts/FIREFOX/firefox.sh
 sudo /tmp/update-scripts/CHROME/chrome.sh
 sudo /tmp/update-scripts/GIT/git.sh
 sudo /tmp/update-scripts/BASH-IT/bash-it.sh
+sudo /tmp/update-scripts/GRADLE/gradle.sh
 sudo /tmp/update-scripts/DBEAVER/dbeaver.sh
 
 find /tmp/CLEANUP/ -type f -print0 | xargs -0 dos2unix
