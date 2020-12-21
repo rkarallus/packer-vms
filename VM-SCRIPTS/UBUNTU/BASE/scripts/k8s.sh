c0ca21fcfe141fdf660ea4e173bd6df27151dfe7
Ronny Karallus <ronny.karallus@misterspex.de>
2020-12-22 00:56:45 +0100

    more k8s and disk in basevm

--- VM-SCRIPTS/UBUNTU/BASE/scripts/k8s.sh
+++ VM-SCRIPTS/UBUNTU/BASE/scripts/k8s.sh
@@ -10,3 +10,26 @@ sudo mv /tmp/eksctl /usr/local/bin
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
+
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+# Go for k9s
+cd /tmp
+wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
+
+# K9s
+mkdir -p /home/ubuntu/development
+cd /home/ubuntu/development
+git clone https://github.com/derailed/k9s.git
+cd /home/ubuntu/development/k9s
+make build
+
+# Ktunnel
+cd /home/ubuntu/development
+git clone https://github.com/omrikiei/ktunnel
+cd ktunnel
+CGO_ENABLED=0 go build -ldflags="-s -w"
+sudo mv ./ktunnel /usr/local/bin/ktunnel
